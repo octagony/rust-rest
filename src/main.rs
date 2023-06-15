61780c82b4cd6640990c3116d911264f2b3f43d7
octagony <moctagony@gmail.com>
2023-06-15 20:17:33 +0300

    [FEAT] Add default_service for 404

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use ntex::web;
 use std::io;
+mod services;
+
 #[web::get("/")]
 async fn index()-> &'static str{
     "Hello world!"
 }
 #[ntex::main]
 async fn main() -> io::Result<()> {
-    web::server(|| web::App::new().service(index))
+    web::server(||{
+        web::App::new().default_service(web::route().to(services::default))
+    } )
     .bind(("0.0.0.0",8080))?
     .run()
     .await?;
