4a51304aeba019ab5094524b80d94302366ca9f4
octagony <octagony@pop-os.localdomain>
2023-06-21 19:39:36 +0300

    [FEAT] Described endpoints

--- src/services/dogs.rs
+++ src/services/dogs.rs
@@ -25,16 +25,40 @@ pub async fn create_dog(_dog: web::types::Json<Dog>)->web::HttpResponse{
 	web::HttpResponse::Created().finish()
 }
 
+#[utoipa::path(
+    get,
+    path="/dog/{id}",
+    responses(
+        (status=200, description="Dog found",body=Dog),
+        (status=404, description="Dog not found",body=HttpError)
+    )
+)]
 #[web::get("/api/dogs/{id}")]
 pub async fn get_dog()->web::HttpResponse{
 	web::HttpResponse::Ok().finish()
 }
 
+#[utoipa::path(
+    put,
+    path="/dog/{id}",
+    responses(
+        (status=200, description="Dog updated",body=Dog),
+        (status=404, description="Dog not found",body=HttpError)
+    )
+)]
 #[web::put("/api/dogs/{id}")]
 pub async fn update_dog()->web::HttpResponse{
 	web::HttpResponse::Ok().finish()
 }
 
+#[utoipa::path(
+    delete,
+    path="/dog/{id}",
+    responses(
+        (status=200, description="Dog deleted",body=Dog),
+        (status=404, description="Dog not found",body=HttpError)
+    )
+)]
 #[web::delete("/api/dogs/{id}")]
 pub async fn delete_dog()->web::HttpResponse{
 	web::HttpResponse::Ok().finish()
